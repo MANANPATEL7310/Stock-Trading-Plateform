e1983d9d931aff687186d4e93be6941c9af97401
Manan Patel <mananpatel7310@gmail.com>
2025-11-21 16:32:04 +0530

    implemented data-fetch on frontend and send it from the backend

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,14 @@ if(process.env.Node_ENV!="production"){
 
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-
+import Holding from './model/HoldingsModel.js';
+import Position from './model/PositionsModel.js';
 
 const app=express();
 const PORT=process.env.PORT || 5000;
-
-
 const MONGO_URL = process.env.MONGO_URL;
 
 async function main(){
@@ -25,9 +26,30 @@ main()
   .catch((err) => console.log(err));
 
 
+ app.use(cors());
+ app.use(bodyParser.json());
+
+  app.get('/allHoldings',async(req,res)=>{
+    try{
+      let holdings=await Holding.find({});
+      res.json(holdings);
+    }
+    catch(err){
+      res.json({message:"Server Error"});
+    }
+  })
+
+  app.get('/allPositions',async (req,res)=>{
+    try{
+      let positions=await Position.find({});
+      res.json(positions);
+    }
+    catch(err){
+      res.json({message:"Server Error"});
+    }
+  })  
 
   
-
 
 
 
